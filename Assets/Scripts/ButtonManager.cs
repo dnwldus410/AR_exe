8623295381efaa800d7ea1d771af2924a482b95d
dnwldus410 <69383333+dnwldus410@users.noreply.github.com>
2020-08-22 16:26:19 +0900

    Initial commit

---
+++ Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonManager : MonoBehaviour
+{
+    public GameObject menuSet;
+    private bool m_clk=false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /* if(Application.platform == RuntimePlatform.Android )
+         {
+             if (Input.GetKey(KeyCode.Escape) && !m_clk)
+             {
+                 menuSet.SetActive(true);
+                 m_clk = true;
+             }
+             if (Input.GetKey(KeyCode.Escape) && m_clk)
+             {
+                 menuSet.SetActive(true);
+
+             }
+         }*/
+        if (Input.GetKey(KeyCode.Escape) && !m_clk)
+        {
+            menuSet.SetActive(true);
+            m_clk = true;
+        }
+        if (Input.GetKey(KeyCode.Escape) && m_clk)
+        {
+            menuSet.SetActive(true);
+
+        }
+
+
+    }
+    public void StartButton()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+    public void AgainButton()
+    {
+        menuSet.SetActive(false);
+    }
+    public void QuitButton()
+    {
+        Application.Quit();
+    }
+}
---
+++ Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    public float m_speed=3.0f;
+    private Rigidbody m_myRb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_myRb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            OnLeft();
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            OnRight();
+        }
+    }
+    public void OnLeft()
+    {
+        m_myRb.velocity = new Vector3(-1 * m_speed, 0.0f, 0.0f);
+    }
+    public void OnRight()
+    {
+        m_myRb.velocity = new Vector3(1 * m_speed, 0.0f, 0.0f);
+    }
+}
